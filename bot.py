3582b18537dc812a141f2c412d6fbb63c3d395f6
cartoonix <153723763+cartoonix@users.noreply.github.com>
2026-01-19 22:48:32 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,10 +41,10 @@ RUn = yone.start(bot_token=BOT_TOKEN)
 
 @yone.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
-  await event.reply("**Hey, I'm Yone Mention Bot**, I can Tag almost all members in group or channel \nHit Command - **/help** for more information\n\n",
+  await event.reply("**Hey, I'm TagMaster pro**, I can Tag almost all members in group or channel \nHit Command - **/help** for more information\n\n",
                     buttons=(
-                      [Button.url('Support', 'https://t.me/league_of_bots'),
-                      Button.url('Developer', 'https://github.com/Noob-kittu')]
+                      [Button.url('Support', 'https://t.me/tg_zomooroo'),
+                      Button.url('Developer', 'https://t.me/ZenoVerse_Network')]
                     ),
                     link_preview=False
                    )
@@ -53,11 +53,11 @@ async def start(event):
 
 @yone.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  helptext = "**Help Menu of Yone Mention Bot**\n\nCommand: /Mention\nYou can use this command with text what you want to Tag others.\n`Example: /mention how are you buddy?`\nYou can you this command as a reply to any message. Bot will tag users to that replied messsage."
+  helptext = "**Help Menu of TagMaster pro Bot**\n\nCommand: /Mention\nYou can use this command with text what you want to Tag others.\n`Example: /mention how are you buddy?`\nYou can you this command as a reply to any message. Bot will tag users to that replied messsage."
   await event.reply(helptext,
                     buttons=(
-                      [Button.url('Support', 'https://t.me/league_of_bots'),
-                      Button.url('Developer', 'https://github.com/Noob-kittu')]
+                      [Button.url('Support', 'https://t.me/tg_zomooroo'),
+                      Button.url('Developer', 'https://t.me/ZenoVerse_Network')]
                     ),
                     link_preview=False
                    )
